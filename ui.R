0459f9f0e5049e657d7b6b3d60e8bc1fdf6e794b
INDIA-CHARLIE-ECHO-MAN <43439611+INDIA-CHARLIE-ECHO-MAN@users.noreply.github.com>
2022-06-27 00:53:40 +1000

    unsure but gotta checout

--- ui.R
+++ ui.R
@@ -7,91 +7,7 @@ ui <- fluidPage(
   #navbarPage is top menu bar
   navbarPage("",
 
-            #tabPanel is each tab in the navbarPage
-            # home tab
-             tabPanel(
-               title="Home",
-
-              # navlistPanel is each tab on the side menu for each tabPanel
-               navlistPanel(
-                 tabPanel(
-                   title="About",
-                   "What is OutDeCo?"       
-                 ),
-                 
-                 tabPanel(
-                   title="Differential Analysis",
-                   "information about DE",
-                 ),
-                 
-                 tabPanel(
-                   title="Cluster Genes",
-                   "information about Cluster genes",
-                 ),
-                 
-                 tabPanel(
-                   title="Gene Connectivity",
-                   "information about Gene Connectivity",
-                 ),
-                 
-                 tabPanel(
-                   title="Functional Outliers",
-                   "information about Functional Outliers",
-                 ),
-                 
-                 tabPanel(
-                   title="Gene Set Enrichment Analysis",
-                   "information about Enrichment Analysis",
-                 ),
-                 
-               )
-             ),
-
-            # run DE tab
-             tabPanel(
-               title="Run DE",
-
-               # side panel for upload options
-               dropdown(
-
-                # title of sidepanel
-                 tags$h3("Options"),
-
-                 # inputs in the sidepanel
-                fileInput("file1", "Choose DE File",
-                  accept = c(
-                  "text/csv",
-                  "text/comma-separated-values,text/plain",
-                   ".csv")
-                ),
-
-                # side panel characteristics
-                style = "gradient", icon = icon("cog"),
-                status = "primary", width = "300px",
-                animate = animateOptions(
-                enter = animations$fading_entrances$fadeInLeftBig,
-                exit = animations$fading_exits$fadeOutLeftBig
-                )
-               ),
-
-               navlistPanel(
-                 tabPanel(
-                   title="wilcox",
-                   "wilcox placeholder",
-                 ),
-                 
-                 tabPanel(
-                   title="DESeq",
-                   "DESeq placeholder",
-                 ),
-                 
-                 tabPanel(
-                   title="edgeR",
-                   "edgeR Placeholder",
-                 ),
-               ),
-             ),
-            
+            #tabPanel is each tab in the navbarPage          
             # Assess DE tab
              tabPanel(
               title="Assess DE",
@@ -143,21 +59,6 @@ ui <- fluidPage(
                     )
                   ),
                 ),
-                
-                tabPanel(
-                  title="Gene Connectivity",
-                  "Gene Connectivity Page",
-                ),
-                 
-                 tabPanel(
-                   title="Functional Outliers",
-                   "Functional Outliers Page",
-                 ),
-                 
-                 tabPanel(
-                   title="Gene Set Enrichment Analysis",
-                   "GSE Page",
-                 ),
                ),
              )
   ),
